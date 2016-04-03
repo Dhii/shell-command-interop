5ddb1ef296c1e893f62d9f451f1b74bca84f9ecb
Anton Ukhanev <xedin.unknown@gmail.com>
2016-04-03 17:41:51 +0200

    Added `ConfigurableCommandInterface` interface

---
+++ src/ConfigurableCommandInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dhii\ShellCommandInterop;
+
+/**
+ * Something that represents a shell command that provides means for basic
+ * configuration of its execution parameters.
+ * 
+ * @since [*next-version*]
+ *
+ * @author Dhii Team <development@dhii.co>
+ */
+interface ConfigurableCommandInterface
+{
+    /**
+     * Set the working directory for the process that will be created to run the command.
+     * 
+     * If the path is relative, the implementation is free to imply any sane base path.
+     * 
+     * @since [*next-version*]
+     *
+     * @param string $directoryPath An absolute or relative path to the working directory of the process.
+     */
+    public function setWorkingDirectory($directoryPath);
+
+    /**
+     * Set the environment variables for the process that will be created to run the command.
+     * 
+     * @since [*next-version*]
+     *
+     * @param array|null $environmentVars A map of environment variable names to their values.
+     *                                    If null, the same environment as the current PHP process will be assumed.
+     */
+    public function setEnvironment($environmentVars);
+
+    /**
+     * Set additional options for the process that will be created to run the command.
+     * 
+     * @since [*next-version*]
+     *
+     * @param array $optionVars A map of option names to their values.
+     */
+    public function setOptions($optionVars);
+}
